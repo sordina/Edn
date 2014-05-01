48a1305379244360212f5ecd785629fcafc9967f
Lyndon Maydwell <lyndon@sordina.net>
2014-05-01 11:45:40 +1000

    Init

---
+++ Main.hs
@@ -0,0 +1,23 @@
+
+module Main where
+
+import Print
+import Data.EDN.Parser
+import Text.Groom
+import Data.EDN.Types
+import qualified Data.ByteString.Lazy       as BSL
+import qualified Data.Text.Internal.Builder as B
+import qualified Data.Text.Lazy.IO          as T
+
+removeTag :: TaggedValue -> Value
+removeTag (NoTag v)       = v
+removeTag (Tagged v ns t) = v
+
+main :: IO ()
+main = BSL.getContents >>= run
+
+run :: BSL.ByteString -> IO ()
+run = maybe (return ()) pp . parseMaybe
+
+pp :: TaggedValue -> IO ()
+pp = T.putStr . B.toLazyText . fromValue 0 . removeTag
---
+++ Print.hs
@@ -0,0 +1,92 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Print (fromValue, fromTagged, encode) where
+
+import Data.Monoid (mappend)
+import qualified Data.Text as T
+import Data.Text.Lazy.Builder
+import Data.Text.Lazy.Builder.Int (decimal)
+import Data.Text.Lazy.Builder.RealFloat (realFloat)
+import Data.Text.Lazy.Encoding (encodeUtf8, decodeUtf8)
+import qualified Data.ByteString.Char8 as BS
+import qualified Data.ByteString.Lazy as L
+
+import qualified Data.Vector as V
+import qualified Data.Map as M
+import qualified Data.Set as S
+
+import qualified Data.EDN.Types as E
+import Data.EDN.Types.Class (ToEDN, toEDN)
+
+-- | Encode a Tagged EDN value to a 'Builder'.
+fromTagged :: Int -> E.TaggedValue -> Builder
+fromTagged n (E.NoTag v) = fromValue n v
+fromTagged n (E.Tagged v "" t) = singleton '#' <> string t <> sp <> fromValue n v
+fromTagged n (E.Tagged v ns t) = singleton '#' <> string ns <> singleton '/' <> string t <> sp <> fromValue n v
+
+spaces :: Int -> Builder
+spaces n = fromString $ replicate n ' '
+
+sp :: Builder
+sp = singleton ' '
+
+-- | Encode a raw EDN value to a 'Builder'.
+fromValue :: Int -> E.Value -> Builder
+fromValue _ E.Nil           = "nil"
+fromValue _ (E.Boolean b)   = if b then "true" else "false"
+fromValue _ (E.String t)    = "\"" <> quote t <> "\""
+fromValue _ (E.Character c) = "\\" <> quoteChar c
+fromValue _ (E.Symbol "" v) = string v
+fromValue _ (E.Symbol ns v) = string ns <> "/" <> string v
+fromValue _ (E.Keyword kw)  = ":" <> string kw
+fromValue _ (E.Integer i)   = decimal i
+fromValue _ (E.Floating f)  = realFloat f
+fromValue _ (E.List xs)     = "("  <> fromList xs <> ")"
+fromValue _ (E.Vec xs)      = "["  <> fromList (V.toList xs) <> "]"
+fromValue _ (E.Set xs)      = "#{" <> fromList (S.toList xs) <> "}"
+fromValue n (E.Map as)      = "{"  <> "\n" <> fromAssoc (n+1) (M.assocs as) <> spaces n <> "}"
+
+string :: BS.ByteString -> Builder
+string s = fromLazyText . decodeUtf8 . L.fromChunks $ [s]
+
+quote :: T.Text -> Builder
+quote q = case T.uncons t of
+    Nothing -> fromText h
+    Just (c, t') -> fromText h <> escape c <> quote t'
+    where
+        (h, t) = T.break isEscape q
+        isEscape c = c == '\"' || c == '\\' || c < '\x20'
+        escape '\"' = "\\\""
+        escape '\\' = "\\\\"
+        escape '\n' = "\\n"
+        escape '\r' = "\\r"
+        escape '\t' = "\\t"
+        escape c = singleton c
+
+quoteChar :: Char -> Builder
+quoteChar c = case c of
+    '\n' -> string "newline"
+    '\r' -> string "return"
+    '\t' -> string "tab"
+    ' '  -> string "space"
+    _    -> singleton c
+
+fromList :: [E.TaggedValue] -> Builder
+fromList [] = ""
+fromList (x:[]) = fromTagged 0 x
+fromList (x:xs) = fromTagged 0 x <> sp <> fromList xs
+
+fromAssoc :: Int -> [(E.Value, E.TaggedValue)] -> Builder
+fromAssoc _ [] = ""
+fromAssoc n ((k, v):[]) = spaces n <> fromValue n k <> sp <> fromTagged n v <> "\n"
+fromAssoc n ((k, v):as) = spaces n <> fromValue n k <> sp <> fromTagged n v <> sp <> "\n" <> spaces n <> fromAssoc n as
+
+-- | Serialize a value as a lazy 'L.ByteString'.
+encode :: ToEDN a => a -> L.ByteString
+encode = encodeUtf8 . toLazyText . fromTagged 0 . toEDN
+{-# INLINE encode #-}
+
+(<>) :: Builder -> Builder -> Builder
+(<>) = mappend
+{-# INLINE (<>) #-}
+infixr 6 <>
